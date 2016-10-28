135c950ef53d6715fa9a34fa8ba83594876eca32
Admin <admin@Diegos-MacBook-Pro.local>
2016-10-27 22:11:03 -0600

    Update comments

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ function killBS {
     ps -Af | grep browser-sync | grep -v grep | awk '{print $2}' | xargs kill
 }
 
+# Call kill method
 killBS
 npm run start > /tmp/out.log 2> /tmp/err.log &
 sleep 5 && ./node_modules/.bin/chimp --browser=phantomjs
