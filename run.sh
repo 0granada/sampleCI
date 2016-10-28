ee4fdb16c3fd0bc203bd73c3d9069b1f7b8254ce
Admin <admin@Diegos-MacBook-Pro.local>
2016-10-27 22:32:09 -0600

    Update comments

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ function killBS {
 # Call kill method
 killBS
 npm run start > /tmp/out.log 2> /tmp/err.log &
+# run chimp
 sleep 5 && ./node_modules/.bin/chimp --browser=phantomjs
 # Call kill method
 killBS
